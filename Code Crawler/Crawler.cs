6ffc040608f6f5bea5ad16210d9a1614ac788ef0
Stephane Hamel <shamel@maestro.ca>
2022-08-06 21:33:46 -0400

    Add project files.

---
+++ Code Crawler/Crawler.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Code_Crawler
+{
+    [TestClass]
+    public class Crawler
+    {
+        Type[] _types = Assembly.GetExecutingAssembly().GetTypes();
+
+        [TestMethod]
+        public void Crawl()
+        {
+            foreach (var type in _types)
+            {
+                type.GetMethods()
+            }
+        }
+    }
+}
