653f7fa9bcc89286c48cadd3a3d7f4cf6cdc03f6
Stephane Hamel <shamel@maestro.ca>
2022-08-07 12:48:37 -0400

    Crawl

--- Code Crawler/Crawler.cs
+++ Code Crawler/Crawler.cs
@@ -1,20 +1,29 @@
 ﻿using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Code_Crawler
-{
-    [TestClass]
-    public class Crawler
-    {
-        Type[] _types = Assembly.GetExecutingAssembly().GetTypes();
+namespace Code_Crawler;
 
-        [TestMethod]
-        public void Crawl()
+[TestClass]
+public class Crawler
+{
+    Type[] _types = Assembly.GetExecutingAssembly().GetTypes();
+
+    [TestMethod]
+    public void Crawl()
+    {
+        foreach (var type in _types.Where(x => x != GetType()))
+            Crawl(type);
+    }
+
+    private static void Crawl(Type type)
+    {
+        var obj = Activator.CreateInstance(type);
+
+        foreach(var methodInfo in type.GetMethods())
         {
-            foreach (var type in _types)
-            {
-                type.GetMethods()
-            }
+            methodInfo.Invoke(obj, Array.Empty<object>());
         }
     }
+    
+
 }
